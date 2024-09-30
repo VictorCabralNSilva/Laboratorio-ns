bda848ed7b6e3d8f4096b9a865d00b197d2dcf56
Daniel Silveira <danieldasilvasilveira2002@gmail.com>
2024-09-30 10:49:47 -0300

    Package atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import React, {Suspense} from 'react'
 import { Skeleton } from './containers/layout/Skeleton/Skeleton'
-/*const Skeleton = React.lazy(() => import('./containers/layout/Skeleton/Skeleton').then(module => ({ default: module.Skeleton })));*/
 const Header = React.lazy(() => import('./containers/layout/Header/Header').then(module => ({ default: module.Header })));
 const Inicio = React.lazy(() => import('./containers/layout/Inicio/Inicio').then(module => ({ default: module.Inicio })));
 const Servicos = React.lazy(() => import('./containers/layout/Servicos/Servicos').then(module => ({ default: module.Servicos })));
